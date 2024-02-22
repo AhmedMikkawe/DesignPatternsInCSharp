cfd620495edf5a9f7b20a74e424fb194fa5abb9d
Ahmed Mikkawe <ah.mikkawe@gmail.com>
2024-02-22 10:57:28 +0200

    Basic Implementation of Singleton Design Pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SingletonPattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Counter counter1 = Counter.Create();
+            Counter counter2 = Counter.Create();
+            Console.WriteLine(counter1.count);
+            Console.WriteLine(counter2.count);
+
+            counter1.Add();
+           
+
+            Console.WriteLine(counter1.count);
+            Console.WriteLine(counter2.count);
+
+            counter2.Add();
+
+            Console.WriteLine(counter1.count);
+            Console.WriteLine(counter2.count);
+        }
+    }
+
+}
+
